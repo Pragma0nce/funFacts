57143280ab4e2034632f78dcdb73872ae5e11ebe
Pragma0nce <kobus.coetzeee@gmail.com>
2016-10-20 12:20:17 +0200

    Button now changes the facts view.

--- app/src/main/java/com/coetzee/kobus/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/coetzee/kobus/funfacts/FunFactsActivity.java
@@ -1,14 +1,39 @@
 package com.coetzee.kobus.funfacts;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
+
+    // Declare our view variables
+    private TextView mFactTextView;
+    private Button mShowFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fun_facts);
+        setContentView(R.layout.activity_fun_facts); // which layout file to user
+
+        // Assign the views from the layout file to the variables
+        mFactTextView = (TextView)findViewById(R.id.factTextView);
+        mShowFactButton = (Button)findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // The button was clicked, so update the facts texview with a new fact
+                String fact = "Ostriches can run faster than horses";
+                mFactTextView.setText(fact);
+            }
+        };
+
+        mShowFactButton.setOnClickListener(listener);
+
+
     }
 
 }
